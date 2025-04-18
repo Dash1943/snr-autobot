a3e3aa62458287fbf921ceff5368aa864f288a2e
Dash1943 <yodashyo@gmail.com>
2025-04-18 22:03:01 +0800

    Config/settings.py

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
-TELEGRAM_TOKEN = '在此填入你的 Telegram Bot Token'
-TELEGRAM_CHAT_ID = '在此填入你的 Chat ID'
+class Config:
+    # 新增余额配置
+    BALANCE_COIN = "USDT"  # 监控的币种
+    AUTO_REFRESH_BALANCE = True  # 是否自动刷新余额
+    BALANCE_REFRESH_INTERVAL = 3600  # 余额刷新间隔(秒)
